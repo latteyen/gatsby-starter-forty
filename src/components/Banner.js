484c0478ab66b3f26f788c1dd148aaa1fadcd562
latteyen <38789408+latteyen@users.noreply.github.com>
2019-05-29 22:50:40 +0700

    text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,10 +4,10 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Beautiful Hotel Website</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
+                <p>A responsive site built to increase bookings<br />
                 and released under the Creative Commons.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
