c3bf4edcad499ad68f814c74144e98e5d38d2d9f
latteyen <38789408+latteyen@users.noreply.github.com>
2019-05-29 23:10:04 +0700

    menu change

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,13 +7,13 @@ const Menu = (props) => (
         <div className="inner">
             <ul className="links">
                 <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/landing">Rooms</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/generic">Facilities</Link></li>
                 <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
             </ul>
             <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Get Started</a></li>
-                <li><a href="#" className="button fit">Log In</a></li>
+                <li><a href="#" className="button special fit">Book Now</a></li>
+                <li><a href="#" className="button fit">Contact Us</a></li>
             </ul>
         </div>
         <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
